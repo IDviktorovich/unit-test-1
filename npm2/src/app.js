b16f72b39688cd68957fd34d4b6b16c0ac6a98a9
IDviktorovich <harajukuken1997@gmail.com>
2022-06-28 20:55:05 +0300

    Модули, задание 2

--- npm2/src/app.js
+++ npm2/src/app.js
@@ -17,3 +17,8 @@ import sum from './basic';
 console.log('app worked');
 
 console.log(sum([1, 2]));
+
+const game = new Game();
+game.start();
+
+import Game, {GameSavingData, readGameSaving as loadGame, writeGameSaving as saveGame} from './game.js';
